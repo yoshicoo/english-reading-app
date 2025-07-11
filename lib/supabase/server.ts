79c8d2a6c2073c714f3d464a9829feb6238c9c85
yoshicoo <kosuke.yoshida.90@shiftinc.jp>
2025-07-11 20:00:03 +0900

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,11 +1,10 @@
 import { createServerClient, type CookieOptions } from '@supabase/ssr'
 import { cookies } from 'next/headers'
-import { Database } from '@/types/supabase'
 
 export async function createServerSupabaseClient() {
   const cookieStore = cookies()
 
-  return createServerClient<Database>(
+  return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
     {
